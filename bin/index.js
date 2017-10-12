bd71b32b1f7fc722f2716147bd9f30cc1fafc011
huarxia <liubiao0810@live.cn>
2017-10-12 11:31:43 +0800

    统一log方法

--- bin/index.js
+++ bin/index.js
@@ -7,18 +7,18 @@ var chalkColor = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 
 var package = require('../package.json');
 var name = package.name;
 var version = package.version;
-
+const log = console.log;
 var run= function (obj) {
     if(obj[0] === '-v'){
-        console.log('version is ' + chalk.green(version));
+        log('version is ' + chalk.green(version));
     }else if(obj[0] === '-h'){
-        console.log('Useage:');
-        console.log('  -v --version [show version]');
+        log('Useage:');
+        log('  -v --version [show version]');
     }else{
         switch (obj[0]) {
             case '-ip':
                 var ip = require('../shell/ip').getIp();
-                console.log('local ip is ' + chalk.green(ip));
+                log('local ip is ' + chalk.green(ip));
             break;
             default:
                 readdir();
@@ -29,13 +29,13 @@ var run= function (obj) {
 var readdir = function() {
     fs.readdir(path, function(err, files){
         if(err){
-            return console.log(err);
+            return log(err);
         }
         chalkColorLen = chalkColor.length;
         for(var i = 0; i < files.length; i += 1){
             var n = Math.ceil(Math.random() * chalkColorLen) - 1;
             n = n === 0? 1: n;
-            console.log(chalk[chalkColor[n]](files[i]));
+            log(chalk[chalkColor[n]](files[i]));
         }
     });
 }
